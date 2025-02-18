ad630759a958991dae3947e801f1ca5f7e1c46c5
gshorti <ilazajkin52969@gmail.com>
2025-02-18 21:56:32 +0300

    Add Image to admin panel

--- back-end/Map/routes/models.py
+++ back-end/Map/routes/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 from comments.models import Comment
 
-
 # Create your models here.
+
+class Image(models.Model):
+    image = models.FileField(upload_to="images")
 
 class Routes(models.Model):
     name = models.CharField(max_length=1000, null=True)
     data = models.JSONField(null=True, blank=True, default=dict)
-    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
+    comments = models.ManyToManyField(Comment, null=True, blank=True)
+    images = models.ManyToManyField(Image, null=True)
 
 
     def __str__(self):
         return self.name
 
 
-class Image(models.Model):
-    image = models.FileField(upload_to="images")
