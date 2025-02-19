7d2fb8b9078b7e2cbbb65f1b8b58140fbb550655
Gvinses <timurkhen10@gmail.com>
2025-02-19 10:47:47 +0300

    HTTP request JS

--- front-end/HTTP_request.js
+++ front-end/HTTP_request.js
@@ -4,20 +4,59 @@ class HTTP_request {
     constructor() {
     }
 
-    setRequest(method, url, body) {
-        let xhr = new XMLHttpRequest()
-        xhr.open(method, url, body)
-        xhr.send()
-        xhr.onload = function () {
-            if (xhr.status !== 200) {
-                return xhr.status
-            } else {
-                return xhr.response
-            }
-        }
+    async get(url) {
+        let output = {}
+        
+        await fetch(url, {
+            method: 'get',
+            headers: headers,
+        }).then(response => response.json())
+          .then(data => output = data)
+          .catch(error => output = new Error(error))
+
+        return output
     }
 
-    getData() {
-        this.setRequest('GET', this.allPostURL, {'content-type': 'application/json'})
+    async post(url, body, headers) {
+        let output = {}
+        
+        await fetch(url, {
+            method: "post",
+            headers: headers,
+            body: JSON.stringify(body)
+        }).then(
+            data => output = data
+        ).catch(
+            error => output = new Error(error)
+        )
+        return output
     }
 }
+
+let http = new HTTP_request()
+
+// HEADERS Template
+let headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+// DATA Template
+// let data = {
+//     'name': "qwerty",
+//     'data': {},
+//     'comments': [ 1 ],
+//     'images': [ 1 ]
+// }
+
+// POST request
+// let post = http.post('https://www.kringeproduction.ru/routes/', data, headers)
+// post.then((data) => {
+//     console.log(data)
+// })
+
+// GET request
+// let get = http.get('https://www.kringeproduction.ru/routes/', headers)
+// get.then((data) => {
+//     console.log(data)
+// })
