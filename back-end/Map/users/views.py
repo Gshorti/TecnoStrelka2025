33f9a5ea02186d35b1d958b819347a310ac3101e
gshorti <ilazajkin52969@gmail.com>
2025-02-21 21:16:02 +0300

    fix auth

--- back-end/Map/users/views.py
+++ back-end/Map/users/views.py
@@ -4,13 +4,15 @@ from Map.auth import KPtubeAuthentication
 from .models import User
 from .permissions import UpdateUserPermission
 from .serializers import US
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
+
 # Create your views here.
 
 class UserView(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = US
     authentication_classes = [KPtubeAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, UpdateUserPermission]
+    permission_classes = [IsAuthenticated, UpdateUserPermission]
     filterset_fields = ('name')
     search_fields = ['name']
