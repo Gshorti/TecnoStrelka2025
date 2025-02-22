88b3a0759c6facc5f2038ea77c8194ef1e9a1d82
gshorti <ilazajkin52969@gmail.com>
2025-02-22 12:50:45 +0300

    add create_user url

--- back-end/Map/users/views.py
+++ back-end/Map/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from Map.auth import KPtubeAuthentication
@@ -18,18 +19,6 @@ class UserView(ModelViewSet):
     filterset_fields = ('name')
     search_fields = ['name']
 
-def post(request):
-    name = request.data["name"]
-    email = request.data["email"]
-    users = User.objects.all()
-    for user in users:
-        if user.name == name:
-            return Response("Invalid name", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    for user in users:
-        if user.email == email:
-            return Response("Invalid email", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    serializer = US(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CreateUser(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = US
