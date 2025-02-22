3f731c602167b397becac73b256abdb6884b434b
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-02-22 15:38:13 +0300

    feat: images on home page

--- front-end/request.js
+++ front-end/request.js
@@ -3,16 +3,49 @@ import { HTTP_client } from "./services/httpClient.js";
 let http = new HTTP_client();
 
 window.onload = function() {
+    // Получение маршрутов
     http.getRoutes().then((res) => {
         if (!res) {
             console.log('Bad request');
+            return;
         }
-        const routeElement = document.getElementById('main-div')
-        routeElement.innerHTML = ''
+
+        let routeElement = document.getElementById('main-div');
+        routeElement.innerHTML = ''; // Очищаем маршруты
 
         res.forEach((item) => {
-            console.log(item.name)
             routeElement.innerHTML += `<div class="route-div">${item.name}</div>`
-        })
-    })
+        });
+    });
+
+    http.getImages().then((res) => {
+        if (!res) {
+            console.log('Bad images request');
+            return;
+        }
+
+        let imageElement = document.getElementById('images-or-route');
+        imageElement.innerHTML = ''; // Очищаем предыдущие изображения
+
+        res.forEach((item) => {
+            console.log(item.image);
+
+            let imageUrl = item.image.replace('127.0.0.1:8001/', 'www.kringeproduction.ru/files/');
+
+            // Создаем новый div для изображения
+            let imageDiv = document.createElement('div');
+            imageDiv.className = 'image-div';
+
+            // Создаем элемент img
+            let imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
+            imgElement.alt = imageUrl;
+
+            // Добавляем изображение в div
+            imageDiv.appendChild(imgElement);
+
+            // Добавляем div с изображением в основной контейнер
+            imageElement.appendChild(imageDiv);
+        });
+    });
 }
