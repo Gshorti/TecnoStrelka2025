74b14d2a04769806e9cd816ee32ee4c472b34363
gshorti <ilazajkin52969@gmail.com>
2025-02-22 12:42:44 +0300

    fix user permissions

--- back-end/Map/users/views.py
+++ back-end/Map/users/views.py
@@ -1,5 +1,6 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.response import Response
 from Map.auth import KPtubeAuthentication
 from .models import User
 from .permissions import UpdateUserPermission
@@ -17,7 +18,18 @@ class UserView(ModelViewSet):
     filterset_fields = ('name')
     search_fields = ['name']
 
-    def create(self, request, *args, **kwargs):
-        self.authentication_classes = []
-        self.permission_classes = []
-        super().create(self, request, *args, **kwargs)
+def post(request):
+    name = request.data["name"]
+    email = request.data["email"]
+    users = User.objects.all()
+    for user in users:
+        if user.name == name:
+            return Response("Invalid name", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    for user in users:
+        if user.email == email:
+            return Response("Invalid email", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    serializer = US(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
