593828357628a084698ab3345cc12d2f47432b3d
gshorti <ilazajkin52969@gmail.com>
2025-02-24 17:51:25 +0300

    Super commit

--- back-end/Map/comments/views.py
+++ back-end/Map/comments/views.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import  IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.response import Response
 from Map.auth import KPtubeAuthentication
 from .models import Comment
 from .serializers import CS
@@ -24,6 +25,10 @@ class CreateCommentView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def post(self, request):
-        cm = ComView()
-        res =  cm.create(request)
-        print(res)
+        serializer = CS(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            res = Response(serializer.data, status=status.HTTP_201_CREATED)
+            print(res)
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
